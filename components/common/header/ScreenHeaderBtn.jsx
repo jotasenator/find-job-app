6c01b432f5bea2b563e7f2f35f7f4d968b093aa5
Maykel <jotasenator@gmail.com>
2023-04-06 04:43:47 -0400

    Until 54:38

---
+++ components/common/header/ScreenHeaderBtn.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Image,TouchableOpacity } from 'react-native'
+
+import styles from './screenheader.style'
+
+const ScreenHeaderBtn = ({iconUrl,dimension,handlePress}) => {
+  return (
+    <TouchableOpacity style={ styles.btnContainer } onPress={ handlePress }>
+      <Image
+        resizeMode="cover"
+        source={ iconUrl }
+        style={ styles.btnImg(dimension) } />
+      </TouchableOpacity>
+  )
+}
+
+export default ScreenHeaderBtn
---
+++ constants/images.js
@@ -0,0 +1,5 @@
+import profile from "../assets/images/yo.png";
+
+export default {
+  profile,
+};
