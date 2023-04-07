0c4491f536060ddf8ea3d0bd9612ecfe69e04e81
Maykel <jotasenator@gmail.com>
2023-04-07 03:14:11 -0400

    the end, still the problem with the app because need to refresh

--- app/index.js
+++ app/index.js
@@ -8,6 +8,7 @@ import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components
 const Home = () =>
 {
     const router = useRouter();
+    const [ searchTerm, setSearchTerm ] = useState( "" );
     return (
         <SafeAreaView style={ { flex: 1, backgroundColor: COLORS.lightWhite } }>
             <Stack.Screen
@@ -28,6 +29,15 @@ const Home = () =>
                     padding: SIZES.medium,
                 } }>
                     <Welcome
+                        searchTerm={ searchTerm }
+                        setSearchTerm={ setSearchTerm }
+                        handleClick={ () =>
+                        {
+                            if ( searchTerm )
+                            {
+                                router.push( `/search/${ searchTerm }` );
+                            }
+                        } }
 
                     />
                     <Popularjobs
