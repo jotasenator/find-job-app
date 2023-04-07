43063798776e881f7d9d815c48b071e387d112c1
Maykel <jotasenator@gmail.com>
2023-04-06 22:32:55 -0400

    1:18 Nearbyjobs

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   View,
   Text,
@@ -21,6 +21,14 @@ const Popularjobs = () => {
     num_pages: 1,
   });
   console.log(data);
+
+  const [selectedJob, setSelectedJob] = useState();
+
+  const handleCardPress = () => {
+    router.push(`job-details/${item.job_id}`);
+    setSelectedJob(item.job_id);
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -38,7 +46,13 @@ const Popularjobs = () => {
         ) : (
           <FlatList
             data={data}
-            renderItem={({ item }) => <PopularJobCard item={item} />}
+            renderItem={({ item }) => (
+              <PopularJobCard
+                item={item}
+                selectedJob={selectedJob}
+                handleCardPress={handleCardPress}
+              />
+            )}
             keyExtractor={(item) => item?.job_id}
             contentContainerStyle={{ columnGap: SIZES.medium }}
             horizontal
